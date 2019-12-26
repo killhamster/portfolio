199c8283b64fd5da984ed156fdeabe480f4fc03b
KH <killhamster@gmail.com>
2019-12-26 14:00:47 +0800

    ugh

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}><App /></BrowserRouter>,
+    <HashRouter basename={process.env.PUBLIC_URL}><App /></HashRouter>,
     document.getElementById('root')
 );
 
