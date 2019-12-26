df01241b8e25f50cd17371041f9de1942c6e66ba
KH <killhamster@gmail.com>
2019-12-26 14:02:25 +0800

    Updates

---
+++ precache-manifest.42f654dd34d2e4ea4e6a08e2ee998d59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3553e0da142bf42ac94a65436483a28",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d7acf4078c0070fd969d",
+    "url": "/portfolio/static/css/main.f87b36e2.chunk.css"
+  },
+  {
+    "revision": "dbb76af82938e62f416b",
+    "url": "/portfolio/static/js/2.530501f1.chunk.js"
+  },
+  {
+    "revision": "d7acf4078c0070fd969d",
+    "url": "/portfolio/static/js/main.74731919.chunk.js"
+  },
+  {
+    "revision": "c5306228cc83f6f6271c",
+    "url": "/portfolio/static/js/runtime-main.7676bd13.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.84a437694fb2a9643620f2864ea29edf.js"
+  "/portfolio/precache-manifest.42f654dd34d2e4ea4e6a08e2ee998d59.js"
 );
 
 self.addEventListener('message', (event) => {
